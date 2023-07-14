e8beb14e9c0756bc7fca99c9356561518913fac0
ducquang2 <nducquangg@gmail.com>
2023-07-14 10:41:45 +0700

    feat: update nvim custom config

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -31,7 +31,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -41,7 +41,6 @@ return {
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        "lua_ls",
         "tsserver",
         "eslint"
       },
@@ -56,25 +55,57 @@ return {
     end,
     -- For eslint:
     eslint = function(opts)
-      opts.root_dir = require("lspconfig.util").root_pattern("package.json", ".eslintrc.json", ".eslintrc.js", ".eslintrc")
+      opts.root_dir = require("lspconfig.util").root_pattern("package.json", ".eslintrc.json", ".eslintrc.js",
+        ".eslintrc")
       return opts
-    end, 
+    end,
+
     setup_handlers = {
       eslint = function(_, opts)
-        require("lspconfig").eslint.setup { 
+        require("lspconfig").eslint.setup {
           on_attach = function(client, bufnr)
             vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = bufnr,
               command = "EslintFixAll",
             })
           end
-        } 
+        }
       end
     },
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
     },
+  },
+
+  highlights = {
+    -- set highlights for all themes
+    -- use a function override to let us use lua to retrieve colors from highlight group
+    -- there is no default table so we don't need to put a parameter for this function
+    init = function()
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+      -- get highlights from highlight groups
+      local normal = get_hlgroup "Normal"
+      local fg, bg = normal.fg, normal.bg
+      local bg_alt = get_hlgroup("Visual").bg
+      local green = get_hlgroup("String").fg
+      local red = get_hlgroup("Error").fg
+      -- return a table of highlights for telescope based on colors gotten from highlight groups
+      return {
+        TelescopeBorder = { fg = bg_alt, bg = bg },
+        TelescopeNormal = { bg = bg },
+        TelescopePreviewBorder = { fg = bg, bg = bg },
+        TelescopePreviewNormal = { bg = bg },
+        TelescopePreviewTitle = { fg = bg, bg = green },
+        TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+        TelescopePromptNormal = { fg = fg, bg = bg_alt },
+        TelescopePromptPrefix = { fg = red, bg = bg_alt },
+        TelescopePromptTitle = { fg = bg, bg = red },
+        TelescopeResultsBorder = { fg = bg, bg = bg },
+        TelescopeResultsNormal = { bg = bg },
+        TelescopeResultsTitle = { fg = bg, bg = bg },
+      }
+    end,
   },
 
   -- Configure require("lazy").setup() options
@@ -132,41 +163,6 @@ return {
     Search = "",
     Selected = "",
     TabClose = "",
-  },
-  plugins = {
-    {
-      "onsails/lspkind.nvim",
-      opts = function(_, opts)
-        -- use codicons preset
-        opts.preset = "codicons"
-        -- set some missing symbol types
-        opts.symbol_map = {
-          Array = "",
-          Boolean = "",
-          Key = "",
-          Namespace = "",
-          Null = "",
-          Number = "",
-          Object = "",
-          Package = "",
-          String = "",
-        }
-        return opts
-      end,
-    },
-    "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = {
-        ensure_installed = { "graphql", "heex", "elixir" }
-      }
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = { "tsserver", "eslint", "elixirls" }, -- automatically install lsp
-      },
-    },
   },
 
   -- Configure require("gitsigns").setup()
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,38 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    "onsails/lspkind.nvim",
+    opts = function(_, opts)
+      -- use codicons preset
+      opts.preset = "codicons"
+      -- set some missing symbol types
+      opts.symbol_map = {
+        Array = "",
+        Boolean = "",
+        Key = "",
+        Namespace = "",
+        Null = "",
+        Number = "",
+        Object = "",
+        Package = "",
+        String = "",
+      }
+      return opts
+    end,
+  },
+  "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "graphql", "heex", "elixir" }
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "tsserver", "eslint", "elixirls" }, -- automatically install lsp
+    },
+  },
 }
